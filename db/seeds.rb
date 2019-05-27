6e7522970466399334671450405fb98853a2026b
Robert Laws <roblaws@me.com>
2019-05-27 08:03:08 +0300

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# seeds.rb
+
+Author.delete_all
 
 authors = [
   {first_name: "Bob", last_name: "Cobb", birth_year: 1974}, 

43ea06bba1700b932457cefc36b0ed656e25ee02
Robert Laws <roblaws@me.com>
2019-05-28 21:05:16 +0300

    enhance authors index show

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   resources :authors, only: [:index, :show, :new, :create]
   resources :book_genres, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :books, only: [:index, :show]
 end

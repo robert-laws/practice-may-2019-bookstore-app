e973601381ccef24c84b427a138f7d7b79b8642a
Robert Laws <roblaws@me.com>
2019-05-30 07:41:38 +0300

    add books form for new action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,11 +8,31 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+    @authors = Author.sorted
+    @book_genres = BookGenre.sorted
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      flash[:notice] = "success|The book has been successively saved to the database."
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def delete
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :publication_year, :description, :price, :author_id, :book_genre_id)
   end
 end

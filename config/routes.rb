2138065f34b9ad511b4e44a7d755650f5b0ef4f1
Robert Laws <roblaws@me.com>
2019-06-01 08:16:01 +0300

    update for custom route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Root Route
   root 'public#index'
 
+  get "authors/year/:year", to: "authors#year", as: "year"
+
   resources :authors, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :book_genres, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :books, only: [:index, :show, :new, :create, :edit, :update]
